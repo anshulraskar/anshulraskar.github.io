3dfe02637f9d906a898384b4e7c50e2a8ebef604
Epic1Online <43654580+Epic1Online@users.noreply.github.com>
2021-04-28 03:56:11 -0400

    Added custom animations

---
+++ js/animations.js
@@ -0,0 +1,31 @@
+var pageContent = document.querySelector(".page-content");
+var navbar = document.querySelector(".navbar");
+var prevScrollPos = pageContent.scrollTop;
+const pageTop = pageContent.scrollTop;
+const pageBottom = pageTop + $(window).height();
+
+pageContent.onscroll = function() {
+    /* Navbar */
+    var currentScrollPos = pageContent.scrollTop;
+    if (currentScrollPos > prevScrollPos) {
+        navbar.style.top = `-${navbar.clientHeight}px`;
+    } else {
+        navbar.style.top = "0";
+    }
+    prevScrollPos = currentScrollPos;
+    /* End of navbar */
+
+
+    $(".fade-scroll").each(function() {
+        if (pageTop <= $(this).position().top && $(this).position().top + $(this).height() <= pageBottom) {
+            $(this).animate({
+                opacity: '1 '
+            }, .8, "swing");
+
+        } else {;
+            $(this).animate({
+                opacity: '.1'
+            }, .8);
+        }
+    });
+}
