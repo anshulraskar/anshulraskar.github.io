cd77b820ed9602dcc9f8ceec81f7f94721ed67fc
Epic1Online <43654580+Epic1Online@users.noreply.github.com>
2021-04-28 18:45:57 -0400

    Animation changes and added more images

--- js/animations.js
+++ js/animations.js
@@ -3,31 +3,49 @@ var navbar = document.querySelector(".navbar");
 var prevScrollPos = pageContent.scrollTop;
 const pageTop = pageContent.scrollTop;
 const pageBottom = pageTop + $(window).height();
+var currentElement = $(".title-header");
+var nextElement = currentElement.next();
 
 pageContent.onscroll = function() {
-    /* Navbar */
     var currentScrollPos = pageContent.scrollTop;
+
+    /* Navbar */
     if (currentScrollPos > prevScrollPos) {
         navbar.style.top = `-${navbar.clientHeight}px`;
     } else {
         navbar.style.top = "0";
     }
-    prevScrollPos = currentScrollPos;
     /* End of navbar */
 
+    /* Custom scrolling */
+    // if (currentScrollPos > prevScrollPos) {
+    //     /* scroll down */
+    //     if (currentElement == $(".title-header")) {
+    //         // pageContent.scrollTo({
+    //         //     top: nextElement.position().top - (nextElement.height() * 2 / 3),
+    //         //     behavior: "smooth"
+    //         // });
+    //     }
+    // } else {
+    //     /* scroll up */
 
+    // }
+    /* End of custom scrolling */
+
+    /*  Fade scroll animation */
     $(".fade-scroll").each(function() {
         if (pageTop <= $(this).position().top && $(this).position().top + $(this).height() <= pageBottom) {
             $(this).addClass("visible");
-            // $(this).animate({
-            //     opacity: '1 '
-            // }, .8, "swing");
+            $(this).removeClass("passed");
 
+        } else if ($(this).position().top + $(this).height() <= pageBottom && !$(this).hasClass("title-header")) {
+            $(this).addClass("passed");
         } else {
             $(this).removeClass("visible");
-            // $(this).animate({
-            //     opacity: '.1'
-            // }, .8);
+
         }
     });
+    /* End of fade scroll animation */
+
+    prevScrollPos = currentScrollPos;
 }
