22af66bde2e655ace46779bad7107087f83514c8
Epic1Online <43654580+Epic1Online@users.noreply.github.com>
2021-05-01 20:08:47 -0400

    Added fadeout page transition

--- js/animations.js
+++ js/animations.js
@@ -19,10 +19,11 @@ pageContent.onscroll = function() {
     $(".fade-scroll").each(function() {
 
         if ($(this).hasClass("title-header") && $(this).position().top <= pageTop && $(this).position().top + $(window).height() >= pageTop) {
-            if ($(this).position().top + $(window).height() >= pageBottom / 3. && currentScrollPos <= prevScrollPos5) {
+            if ($(this).position().top + $(window).height() >= pageBottom / 3.5 /*&& currentScrollPos <= prevScrollPos*/ ) {
                 $(this).addClass("visible");
             }
-        } else if (pageTop <= $(this).position().top && $(this).position().top + $(this).height() <= pageBottom) {
+        }
+        if (pageTop <= $(this).position().top && $(this).position().top + $(this).height() <= pageBottom) {
             $(this).addClass("visible");
             $(this).removeClass("passed");
 
@@ -30,10 +31,22 @@ pageContent.onscroll = function() {
             $(this).addClass("passed");
         } else {
             $(this).removeClass("visible");
-
         }
     });
     /* End of fade scroll animation */
 
     prevScrollPos = currentScrollPos;
 }
+
+$(".link").click(function(event) {
+    event.preventDefault();
+    var href = $(this).attr('href');
+
+    $(".nav-container").slideUp(250);
+    $(".page-content").fadeOut(250, function() {
+        // go to link when animation completes
+        window.location = href;
+
+    })
+
+});
